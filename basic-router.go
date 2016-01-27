a1a971913ff43d25d9eafc9b5a7cd4111df73291
maxwell <maxwel92@126.com>
2016-01-27 10:07:41 +0800

    5th commit

--- basic-router.go
+++ basic-router.go
@@ -8,13 +8,25 @@ import (
 )
 
 func main() {
-//    router := mux.NewRouter().StrictSlash(true)
-    router := mux.NewRouter().StrictSlash(false)
+    router := mux.NewRouter().StrictSlash(true)
     router.HandleFunc("/", Index)
+    router.HandleFunc("/mushroom", Mushroom)
+    router.HandleFunc("/magic/{magicID}", MagicShow)
+    
     log.Fatal(http.ListenAndServe(":8080", router))
  
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "hello, %q", html.EscapeString(r.URL.Path))
+}
+
+func Mushroom(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "hello mushroomo")
+}
+
+func MagicShow(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    magicID := vars["magicID"]
+    fmt.Fprintln(w, "MagicShow", magicID)
 }
